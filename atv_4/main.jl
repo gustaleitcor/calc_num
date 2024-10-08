b838ab38de654a8dbe8be8ca373d27b932bd6262
gustaleitcor <gustavo.leitecordeiro@gmail.com>
2024-10-08 16:30:38 -0300

    q3, q4

--- atv_4/main.jl
+++ atv_4/main.jl
@@ -1,14 +1,16 @@
 include("q1.jl")
 include("q2.jl")
 include("q3.jl")
+include("q4.jl")
 # Import necessary modules
 using Printf
 
 # Define main function
 function main()
-    q_um()
-    q_dois()
-    q_tres()
+	q_um()
+	q_dois()
+	q_tres()
+	q_quatro()
 end
 
 # Call the main function
--- atv_4/q3.jl
+++ atv_4/q3.jl
@@ -3,19 +3,21 @@ using LinearAlgebra
 function q_tres()
 	x = [0.0, 0.0, 0.0]
 	b = [1.0, 4.0, 0.0]
+	m = [3 -1 1; 3 3 7; 3 6 2]
 
-    m = [3 -1 1; 3 3 7; 3 6 2]
+	m[2,:], m[3,:] = m[3,:], m[2,:]
 
-	fx1(x2, x3) = (1 - x3 + x2) / 3
-	fx2(x1, x3) = (4 - 7 * x3 - 3 * x1) / 3
-	fx3(x1, x2) = (-3 * x1 - 6 * x2) / 2
+	fx1(x2, x3) = (b[1] - m[1, 2]x2 - m[1, 3]x3) / m[1, 1]
+	fx2(x1, x3) = (b[2] - m[2, 1]x1 - m[2, 3]x3) / m[2, 2]
+	fx3(x1, x2) = (b[3] - m[3, 1]x1 - m[3, 2]x2) / m[3, 3]
 
-    while(norm(x-b) > 0.001)
-        println(norm(x-b), " ", x)
-        x[1] = fx1(x[2], x[3])
-        x[2] = fx2(x[1], x[3])
-        x[3] = fx3(x[1], x[2])
-    end
+	while (norm(m * x - b) > 0.0001)
+		x[1] = fx1(x[2], x[3])
+		x[2] = fx2(x[1], x[3])
+		x[3] = fx3(x[1], x[2])
+	end
+
+	println("x1: ", x[1], " | x2: ", x[2], " | x3: ", x[3], " => norma: ", norm(m*x - b))
 end
 
 export q_tres
